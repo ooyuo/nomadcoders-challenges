959d3a9137826d4268317dd75ab311f2ebe38d17
doyounjeong <doyounjeong0@gmail.com>
2020-03-12 23:13:45 +0900

    Is It Down?

---
+++ wetube-codeChallenge-07/src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import request from "request";
+import status from "./status";
+
+const app = express();
+
+
+app.get("/", async (req, res) => {
+    let {
+        query: { url }
+    } = req;
+
+    if ( !url.includes("http") ) {
+        url = `http://${url}`;
+    }
+    
+    await request(url, function (error, response, body) {
+      
+        if (response && response.statusCode <= 445) {
+            status.statusCode = "UP"
+            res.send(status.statusCode);
+        } else {
+            status.statusCode = "DOWN";
+            res.send(status.statusCode);
+        }
+    });
+});
+
+// Codesanbox does not need PORT :)
+app.listen(4000, () => console.log(`Listening!`));
