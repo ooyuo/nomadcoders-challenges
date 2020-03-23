4c8670dbfb79e46385b29c7b64bbd1b46f4b7c15
doyounjeong <doyounjeong0@gmail.com>
2020-03-23 23:07:48 +0900

    Coin Tracker

--- coin-tracker-blueprint/src/index.js
+++ coin-tracker-blueprint/src/index.js
@@ -1,12 +1,28 @@
 const API_URL = "https://api.coinpaprika.com/v1/tickers";
-const coinTracker = document.getElementById("coin-tracker__list");
+const coinList = document.getElementById("coin-tracker__list");
 
-fetch(API_URL)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-    const coins = JSON.parse(myJson);
-    console.log(coins);
-  });
+function fetchData() {
+  fetch(API_URL)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      let stringJson = JSON.stringify(myJson);
+      let coinObj = JSON.parse(stringJson);
+      //console.log(coinObj);
+      for (let i = 0; i < coinObj.length; i++) {
+        const li = document.createElement("li");
+        coinList.appendChild(li);
+        li.innerHTML = `${coinObj[i].name}: ${coinObj[i].quotes.USD.price}`;
+      }
+    });
+}
+function setInter() {
+  setInterval(fetchData, 5000);
+}
+
+function init() {
+  fetchData();
+  setInter();
+}
+init();
